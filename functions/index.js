1ddf4b36e949e5aa8523de7cf85f6d2ff8e079fb
Alex <flalex855@gmail.com>
2021-02-10 12:19:33 +0300

    optimize

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,58 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 const db = admin.firestore();
 
-const docRef = db.collection('users').doc('alovelace');
+// POST
 
-const userSet = async() => {
-await docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
+app.post("/users", async (req, res) => {
+  try {
+    const newUser = {
+      ...req.body,
+    };
+
+    // Here we connect to our collection FireStore
+    const newUserDocument = db.collection("users").doc(`${newUser.name}`);
+
+    // Set new user into collection
+    console.log(newUserDocument);
+
+    await newUserDocument
+      .set(newUser)
+      .then(() => res.status(201).send(newUser));
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+  // Response new
 });
 
-const aTuringRef = db.collection('users').doc('aturing');
+//GET
 
-await aTuringRef.set({
-  'first': 'Alan',
-  'middle': 'Mathison',
-  'last': 'Turing',
-  'born': 1912
-});
-}
+app.get("/users", async (req, res) => {
+  try {
+    // Here we connect to our collection FireStore and get all values
+    const snapshot = await db.collection("users").get(); 
 
-userSet()
-
-app.get("/app", async (req,res) => {
-    const snapshot = await db.collection('users').get();
     snapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-});
-})
+        console.log('DOC!!! ', doc);
+      console.log(doc.id, "=>", doc.data());
+    });
 
-exports.app = functions.https.onRequest(app)
+    res.status(201).send(snapshot);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete("/users/:name", async (req, res) => {
+  const { name } = req.params;
+  console.log(name);
+  await db.collection("users").doc(`${name}`).delete();
+
+  res.status(201).send(`${name} deleted`);
+});
+
+exports.app = functions.https.onRequest(app);
